a603fdf3fe3b0df7d8cb2c00f4c79dc4effe2131
Hayashi Yoshiaki <star-yoshi@i.softbank.jp>
2022-09-19 15:10:41 +0900

    refactored

--- plaac.py
+++ plaac.py
@@ -18,6 +18,247 @@ from matplotlib.colors import ListedColormap, BoundaryNorm
 import os
 os.system('')
 
+class Sequence():
+	def __init__(self,name,seq,fasta):
+		self.name = name
+		self.seq = seq
+		if self.seq[-1] == '*':
+			self.seq = self.seq[:-1]
+		
+		self.length = len(self.seq)
+		self.indices = AA.stringToIndices(seq)
+		if len(self.indices) < 1:
+			print(name + ' has no seqence')
+			return
+		
+		self.hmm = fasta.hmm
+		self.hmmRef = fasta.hmmRef
+		
+		self.indicesMW = AA.table['mask'][self.indices]
+		self.sizeMW = 80
+		if len(self.indicesMW) < 80:
+			self.sizeMW = len(self.indicesMW)
+		self.scoreMW = highestScoringSubsequence(self.indicesMW,self.sizeMW,self.sizeMW)
+		
+		self.indicesLLR = llr[self.indices]
+		self.sizeLLR = fasta.coreLength
+		self.scoreLLR = highestScoringSubsequence(self.indicesLLR,self.sizeLLR,self.sizeLLR)
+		
+		self.hmm.decodeAll(self.indices)
+		self.hmmRef.decodeAll(self.indices)
+		
+		self.hmmScore = self.hmm.lmarginalProb - self.hmmRef.lmarginalProb
+		self.hmmScoreV = self.hmm.lviterbiProb - self.hmmRef.lviterbiProb
+		
+		self.disorderReport = DisorderReport(self.indices,fasta.ww1,fasta.ww2,fasta.ww3,np.array([2.785,-1,-1.151]),fasta.llr,AA.table['lodpapa1'])
+		self.viterbiPath = self.hmm.viterbiPath
+		self.longestPrd = longestOnes(self.viterbiPath)
+		
+		self.indicesCORE = fasta.llr[self.indices]
+		bigNeg = -1e+6
+		self.indicesCORE[self.viterbiPath==0] = bigNeg
+		
+		self.scoreCORE = highestScoringSubsequence(self.indicesCORE,fasta.coreLength,fasta.coreLength)
+		
+		self.coreStart = self.scoreCORE[0]
+		self.coreStop = self.scoreCORE[1]
+		
+		self.aaStart = self.coreStart
+		self.aaStop = self.coreStop
+		
+		if(self.scoreCORE[2] > bigNeg/2):
+			while self.aaStart >= 0 and self.viterbiPath[self.aaStart] == 1:
+				self.aaStart -= 1
+			self.aaStart += 1
+			while self.aaStop < len(self.viterbiPath) and self.viterbiPath[self.aaStop] == 1:
+				self.aaStop += 1
+			self.aaStop -= 1
+			self.indicesPRD = self.indices[self.aaStart:self.aaStop]
+			self.scorePRD = np.sum(llr[self.indicesPRD])
+		else:
+			self.scoreCORE[2] = np.nan
+			self.aaStart = -1
+			self.aaStop = -2
+			self.coreStart = -1
+			self.coreStop = -2
+			self.PRD = []
+			self.PRDScore = 0
+	
+	def print(self):
+		print(
+			tabulate(
+				[
+					[
+						Colors.CYAN + self.name + Colors.RESET,
+						Colors.YELLOW + 'Score' + Colors.RESET,
+						Colors.YELLOW  + 'Start' + Colors.RESET,
+						Colors.YELLOW  + 'End' + Colors.RESET,
+						Colors.YELLOW  + 'Length' + Colors.RESET
+						
+					],
+					[
+						Colors.YELLOW + 'MW' + Colors.RESET,
+						int(self.scoreMW[2]),
+						self.scoreMW[0]+1,
+						self.scoreMW[1]+1,
+						self.scoreMW[1]-self.scoreMW[0]+1
+					],
+					[
+						Colors.YELLOW + 'LLR' + Colors.RESET,
+						int(self.scoreLLR[2]),
+						self.scoreLLR[0]+1,
+						self.scoreLLR[1]+1,
+						self.scoreLLR[1]-self.scoreLLR[0]+1
+					],
+					[
+						Colors.YELLOW + 'CORE' + Colors.RESET,
+						int(self.scoreCORE[2]),
+						self.coreStart+1,
+						self.coreStop+1,
+						self.coreStop - self.coreStart + 1
+					
+					],
+					[
+						Colors.YELLOW + 'PRD' + Colors.RESET,
+						int(self.scorePRD),
+						self.aaStart+1,
+						self.aaStop+1,
+						self.aaStop - self.aaStart + 1
+					]
+				],
+				headers="firstrow",
+				floatfmt=".2f"
+			)
+		)
+		print()
+		print(
+			tabulate(
+				[
+					[Colors.YELLOW + 'NLLR' + Colors.RESET,self.scoreLLR[2] / (self.scoreLLR[1]-self.scoreLLR[0]+1)],
+					[Colors.YELLOW + 'VIT maxrun' + Colors.RESET,self.longestPrd],
+					[Colors.YELLOW + 'PRDT len' + Colors.RESET,self.length],
+					[Colors.YELLOW + 'HMM all' + Colors.RESET,self.hmmScore],
+					[Colors.YELLOW + 'HMM vit' + Colors.RESET,self.hmmScoreV],
+					[Colors.YELLOW + 'FI num' + Colors.RESET,self.disorderReport.numDisorderedStrict2],
+					[Colors.YELLOW + 'FI hydro' + Colors.RESET,self.disorderReport.meanHydro],
+					[Colors.YELLOW + 'FI charge' + Colors.RESET,self.disorderReport.meanCharge],
+					[Colors.YELLOW + 'FI combo' + Colors.RESET,self.disorderReport.meanFI],
+					[Colors.YELLOW + 'FI max run' + Colors.RESET,self.disorderReport.maxLengthFI],
+					[Colors.YELLOW + 'PAPA combo' + Colors.RESET,self.disorderReport.papaMaxScore],
+					[Colors.YELLOW + 'PAPA prop' + Colors.RESET,self.disorderReport.papaMaxProb],
+					[Colors.YELLOW + 'PAPA FI' + Colors.RESET,self.disorderReport.papaMaxDis],
+					[Colors.YELLOW + 'PAPA LLR' + Colors.RESET,self.disorderReport.papaMaxLLR,],
+					[Colors.YELLOW + 'PAPA LLR2' + Colors.RESET,self.disorderReport.papaMaxLLR2],
+					[Colors.YELLOW + 'PAPA cen' + Colors.RESET,self.disorderReport.papaMaxCenter + 1]
+				],
+				floatfmt=".2f"
+			)
+		)
+		print()
+		
+	def plot(self,ax,length):
+		ax.imshow(AA.stringToColorIndices(self.seq)[np.newaxis,:],cmap='jet',aspect=20)
+		ax.axes.get_xaxis().set_ticks([])
+		ax.axes.get_yaxis().set_ticks([])
+		ax.yaxis.set_label_coords(-0.05,0)
+		ax.set_xlim([0,length])
+		ax.set_ylim([-0.7,0.7])
+		ax.set_ylabel(self.name,rotation=0)
+		
+		ax.spines['top'].set_visible(False)
+		ax.spines['right'].set_visible(False)
+		ax.spines['bottom'].set_visible(False)
+		ax.spines['left'].set_visible(False)
+		ax.set_aspect('auto')
+	
+		cmap = ListedColormap(['k','r'])
+		norm = BoundaryNorm([0,1],cmap.N)
+		
+		x = np.arange(self.length)
+		y = 0.65 * np.ones(self.length)
+		
+		points = np.array([x,y]).T.reshape(-1,1,2)
+		segments = np.concatenate([points[:-1],points[1:]],axis=1)
+		lc = LineCollection(segments,cmap=cmap,norm=norm)
+		lc.set_array(self.hmm.mapPath)
+		lc.set_linewidth(6)
+		ax.add_collection(lc)
+		
+		y = -0.65 * np.ones(len(self.seq))
+		
+		points = np.array([x,y]).T.reshape(-1,1,2)
+		segments = np.concatenate([points[:-1],points[1:]],axis=1)
+		lc = LineCollection(segments,cmap=cmap,norm=norm)
+		lc.set_array(self.hmm.viterbiPath)
+		lc.set_linewidth(6)
+		ax.add_collection(lc)
+
+
+class Fasta():
+	def __init__(self,inputFile,coreLength,ww1,ww2,ww3,fg,bg):
+		self.hmm = HiddenMarkovModel.prionHMM1(fg,bg)
+		self.hmmRef = HiddenMarkovModel.prionHMM0(bg)
+		self.llr = np.log(fg/bg)
+		self.coreLength = coreLength
+		self.ww1 = ww1
+		self.ww2 = ww2
+		self.ww3 = ww3
+		
+		self.fasta = readFasta(inputFile)
+		self.fastaLen = len(self.fasta)
+		self.seqLenMax = np.max([len(fs[1]) for fs in self.fasta])
+		
+		self.fig, self.ax = plt.subplots(nrows=self.fastaLen,sharex=True,figsize=(12,3))
+		self.scalarMap = plt.cm.ScalarMappable(
+			cmap = plt.get_cmap('jet',len(AA.ctable)),
+			norm = matplotlib.colors.BoundaryNorm(np.arange(len(AA.ctable)+1)-0.5,len(AA.ctable))
+		)
+		self.scalarMap.set_array([])
+		self.colorBar = self.fig.colorbar(self.scalarMap,ax=self.ax,
+			ticks=np.arange(len(AA.ctable)),
+			location='top',
+			shrink=0.8,
+			orientation='horizontal'
+		)
+		self.colorBar.set_ticklabels(AA.ctable)
+		
+	def print(self,plotDir):
+		if plotDir is not None:
+			os.makedirs(plotDir,exist_ok=True)
+		
+		for i,fs in enumerate(self.fasta):
+			seq = Sequence(*fs,self)
+			seq.print()
+			seq.plot(self.ax[i],self.seqLenMax)
+			if plotDir is not None:
+				figp,axp = plt.subplots(nrows=3,sharex=True,figsize=(12,6),gridspec_kw={'height_ratios': [1,0.2,1]})
+				#axp[0].set_ylim([0,0.01])
+				for i in range(seq.hmm.ns):
+					axp[0].plot(normalize2(seq.hmm.postProb[i]),label=seq.hmm.names[i])
+				
+				axp[0].set_xlim([0,seq.length])
+				axp[0].set_ylim([-0.01,1.01])
+				axp[0].set_yticks([0,1])
+				axp[0].set_yticklabels(seq.hmm.states)
+				axp[0].legend(loc=(1.04,0.5))
+				
+				seq.plot(axp[1],seq.length)
+				
+				axp[2].plot(seq.disorderReport.fi,label='FoldIndex')
+				axp[2].fill_between(np.arange(seq.length),seq.disorderReport.fi,alpha=0.7)
+				axp[2].plot(-seq.disorderReport.plaacLLR,label='-PLAAC')
+				axp[2].plot(- 4 * seq.disorderReport.papa,label='-4*PAPA')
+				
+				axp[2].set_xlim([0,seq.length])
+				axp[2].set_ylim([-1,1])
+				axp[2].legend(loc=(1.04,0.5))
+				
+				path = os.path.join(plotDir,seq.name + '.png')
+				plt.subplots_adjust(right=0.85)
+				figp.savefig(path)
+				plt.close(figp)
+		plt.show()
+	
 def normalize(array):
 	sum = array.sum()
 	if sum == 0:
@@ -79,234 +320,6 @@ def longestOnes(seq):
 		else:
 			i += 1
 	return maxl
-
-#@profile
-
-def plotSeq(ax,name,seq,seqlenmax,mapPath,viterbiPath):
-	ax.imshow(AA.stringToColorIndices(seq)[np.newaxis,:],cmap='jet',aspect=20)
-	ax.axes.get_xaxis().set_ticks([])
-	ax.axes.get_yaxis().set_ticks([])
-	ax.yaxis.set_label_coords(-0.05,0)
-	ax.set_xlim([0,seqlenmax])
-	ax.set_ylim([-0.7,0.7])
-	ax.set_ylabel(name,rotation=0)
-	
-	ax.spines['top'].set_visible(False)
-	ax.spines['right'].set_visible(False)
-	ax.spines['bottom'].set_visible(False)
-	ax.spines['left'].set_visible(False)
-	ax.set_aspect('auto')
-
-	cmap = ListedColormap(['k','r'])
-	norm = BoundaryNorm([0,1],cmap.N)
-	
-	x = np.arange(len(seq))
-	
-	y = 0.65 * np.ones(len(seq))
-	points = np.array([x,y]).T.reshape(-1,1,2)
-	segments = np.concatenate([points[:-1],points[1:]],axis=1)
-	lc = LineCollection(segments,cmap=cmap,norm=norm)
-	lc.set_array(mapPath)
-	lc.set_linewidth(6)
-	ax.add_collection(lc)
-	
-	y = -0.65 * np.ones(len(seq))
-	points = np.array([x,y]).T.reshape(-1,1,2)
-	segments = np.concatenate([points[:-1],points[1:]],axis=1)
-	lc = LineCollection(segments,cmap=cmap,norm=norm)
-	lc.set_array(viterbiPath)
-	lc.set_linewidth(6)
-	ax.add_collection(lc)
-
-def scoreAllFastas(file,coreLength,ww1,ww2,ww3,fg,bg,llr,hmm1,hmm0,plotDir=None):
-	print(
-		"SEQid\tMW\tMWstart\tMWend\tMWlen\tLLR\tLLRstart\tLLRend\t"\
-		"LLRlen\tNLLR\tVITmaxrun\tCOREscore\tCOREstart\tCOREend\tCORElen\tPRDscore\tPRDstart\tPRDend\tPRDlen\tPROTlen\t"\
-		"HMMall\tHMMvit\tCOREaa\tSTARTaa\tENDaa\tPRDaa\tFInumaa\tFImeanhydro\tFImeancharge\tFImeancombo\tFImaxrun\t"\
-		"PAPAcombo\tPAPAprop\tPAPAfi\tPAPAllr\tPAPAllr2\tPAPAcen\tPAPAaa"
-	)
-	print()
-	
-	fasta = readFasta(file)
-	
-	### preparation for plot
-	
-	fastalen = len(fasta)
-	seqlenmax = 0
-	for fs in fasta:
-		if len(fs[1]) > seqlenmax:
-			seqlenmax = len(fs[1])
-	fig,ax = plt.subplots(nrows=fastalen,sharex=True,figsize=(12,3))
-	
-	sm = plt.cm.ScalarMappable(
-		cmap = plt.get_cmap('jet',len(AA.ctable)),
-		norm = matplotlib.colors.BoundaryNorm(np.arange(len(AA.ctable)+1)-0.5,len(AA.ctable))
-	)
-	sm.set_array([])
-	cb = fig.colorbar(sm,ax=ax,
-			ticks=np.arange(len(AA.ctable)),
-			location='top',
-			shrink=0.8,
-			orientation='horizontal'
-		)
-	
-	cb.set_ticklabels(AA.ctable)
-
-	###
-	
-	if plotDir is not None:
-		os.makedirs(plotDir,exist_ok=True)
-
-	###
-	
-	for i,fs in enumerate(fasta):
-		name = fs[0]
-		seq = fs[1]
-		if seq[-1] == '*':
-			seq = seq[:-1]
-		
-		aa = AA.stringToIndices(seq)
-
-		if len(aa) < 1:
-			continue
-		maa1 = AA.table["mask"][aa]
-		mwsize = 80
-		if len(maa1) < 80:
-			mwsize = len(maa1)
-		hs1 = highestScoringSubsequence(maa1,mwsize,mwsize)
-		
-		maa2 = llr[aa]
-		hs2 = highestScoringSubsequence(maa2,coreLength,coreLength)
-		
-		hmm1.decodeAll(aa)
-		hmm0.decodeAll(aa)
-		
-		hmmScore = hmm1.lmarginalProb - hmm0.lmarginalProb
-		hmmScoreV = hmm1.lviterbiProb - hmm0.lviterbiProb
-		
-		dr = DisorderReport(aa,ww1,ww2,ww3,np.array([2.785,-1,-1.151]),llr,AA.table['lodpapa1'])
-		mp = hmm1.viterbiPath
-		#longestPrd = int(highestScoringSubsequence(mp)[2])
-		longestPrd = longestOnes(mp)
-		maa3 = llr[aa]
-		big_neg = -1e+6
-		maa3[mp==0] = big_neg
-		hs3 = highestScoringSubsequence(maa3,coreLength,coreLength)
-		
-		coreStart = hs3[0]
-		coreStop = hs3[1]
-		
-		aaStart = coreStart
-		aaStop = coreStop
-		
-		if(hs3[2] > big_neg/2):
-			while aaStart >= 0 and mp[aaStart] == 1:
-				aaStart -= 1
-			aaStart += 1
-			while aaStop < len(mp) and mp[aaStop] == 1:
-				aaStop += 1
-			aaStop -= 1
-			prd = aa[aaStart:aaStop+1]
-			prdScore = np.sum(llr[prd])
-		else:
-			hs3[2] = np.nan
-			aaStart = -1
-			aaStop = -2
-			coreStart = -1
-			coreStop = -2
-			prd = []
-			prdScore = 0
-		
-		fmtstr = (
-			'{}\t' + '{:d}\t'*4 +
-			'{:.3f}\t' + '{:d}\t'*3 +
-			'{:.3f}\t' +
-			Colors.RED + '{:d}\t' + Colors.RESET + 
-			'{:.3f}\t' + '{:d}\t'*3 +
-			'{:.3f}\t' + '{:d}\t'*3 +
-			Colors.RED + '{:d}\t' + Colors.RESET +
-			'{:.3f}\t'*2
-		)
-		
-		str1 = fmtstr.format(
-			Colors.CYAN + name + Colors.RESET,
-			int(hs1[2]),hs1[0] + 1,hs1[1] + 1,hs1[1] - hs1[0] + 1,
-			hs2[2],hs2[0] + 1,hs2[1] + 1,hs2[1] - hs2[0] + 1,
-			hs2[2] / (hs2[1] - hs2[0] + 1),
-			longestPrd,
-			hs3[2],coreStart + 1,coreStop + 1,coreStop - coreStart + 1,
-			prdScore,aaStart + 1,aaStop + 1,aaStop - aaStart + 1,
-			len(aa),
-			hmmScore,
-			hmmScoreV
-		)
-		
-		if aaStop - aaStart + 1 >= coreLength:
-			str2 = (
-				AA.indicesToString(aa[coreStart:coreStop+1]) + '\t' +
-				AA.indicesToString(aa[aaStart:aaStart+14]) + '\t' +
-				AA.indicesToString(aa[aaStop-14:aaStop]) + '\t' +
-				AA.indicesToString(prd)
-			)
-		else:
-			str2 = '-\t-\t-\t-'
-		
-		fmtstr = (
-			'\t{:d}' +
-			'\t{:.3f}'*3 +
-			'\t{:d}' + 
-			'\t{:.3f}'*5 +
-			'\t{:d}\t{}'
-		)
-		
-		str3 = fmtstr.format(
-			dr.numDisorderedStrict2,
-			dr.meanHydro,dr.meanCharge,dr.meanFI,
-			dr.maxLengthFI,
-			dr.papaMaxScore,dr.papaMaxProb,dr.papaMaxDis,dr.papaMaxLLR,dr.papaMaxLLR2,
-			dr.papaMaxCenter + 1,
-			AA.indicesToString(aa[dr.papaMaxCenter-ww2//2:dr.papaMaxCenter+ww2//2+1])
-		)
-		print(str1+str2+str3)
-		
-		###plot
-				
-		plotSeq(ax[i],name,seq,seqlenmax,hmm1.mapPath,hmm1.viterbiPath)
-		
-		if plotDir is not None:
-			figp,axp = plt.subplots(nrows=3,sharex=True,figsize=(12,6),gridspec_kw={'height_ratios': [1,0.2,1]})
-			#axp[0].set_ylim([0,0.01])
-			for i in range(hmm1.ns):
-				axp[0].plot(normalize2(hmm1.postProb[i]),label=hmm1.names[i])
-			
-			axp[0].set_xlim([0,len(seq)])
-			axp[0].set_ylim([-0.01,1.01])
-			axp[0].set_yticks([0,1])
-			axp[0].set_yticklabels(hmm1.states)
-			axp[0].legend(loc=(1.04,0.5))
-			
-			plotSeq(axp[1],name,seq,len(seq),hmm1.mapPath,hmm1.viterbiPath)
-			
-			axp[2].plot(dr.fi,label='FoldIndex')
-			axp[2].fill_between(np.arange(len(seq)),dr.fi)
-			axp[2].plot(-dr.plaacLLR,label='-PLAAC')
-			axp[2].plot(- 4 * dr.papa,label='-4*PAPA')
-			
-			axp[2].set_xlim([0,len(seq)])
-			axp[2].set_ylim([-1,1])
-			axp[2].legend(loc=(1.04,0.5))
-			
-			path = os.path.join(plotDir,name + '.png')
-			plt.subplots_adjust(right=0.85)
-			figp.savefig(path)
-			plt.close(figp)
-			
-		#print(hmm1.mapPath)
-		#print(hmm1.postProb)
-
-		###
-
-	plt.show()
 
 parser = argparse.ArgumentParser(description='plaac')
 parser.add_argument('-i','--inputFile')
@@ -416,10 +429,12 @@ llr = np.log(fg/bg)
 #print(Colors.CYAN + str(bg) + Colors.RESET)
 #print(Colors.BLUE + str(llr) + Colors.RESET)
 
-hmm1 = HiddenMarkovModel.prionHMM1(fg,bg)
-hmm0 = HiddenMarkovModel.prionHMM0(bg)
+#hmm1 = HiddenMarkovModel.prionHMM1(fg,bg)
+#hmm0 = HiddenMarkovModel.prionHMM0(bg)
 
 ww3 = 41
 
 if (args.inputFile is not None):
-	scoreAllFastas(args.inputFile,args.coreLength,args.ww1,args.ww2,ww3,fg,bg,llr,hmm1,hmm0,args.plotDir)
+	#scoreAllFastas(args.inputFile,args.coreLength,args.ww1,args.ww2,ww3,fg,bg,llr,hmm1,hmm0,args.plotDir)
+	fasta = Fasta(args.inputFile,args.coreLength,args.ww1,args.ww2,ww3,fg,bg)
+	fasta.print(args.plotDir)
